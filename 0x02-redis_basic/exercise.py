599d38e4db35fe6dbf4abe335035d5e26e9c1e11
Teepee78 <eniolatoluwalase78@gmail.com>
2023-02-15 16:28:29 +0100

    Update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -67,21 +67,21 @@ def replay(method: Callable):
     Args:
         method (Callable): method
     """
-    redis = redis.Redis()
+    store = redis.Redis()
     key = method.__qualname__
 
     # Print how many times method was called
-    count = redis.get(key)
+    count = store.get(key)
     # try:
     # count = int(count.decode("utf-8"))
     # except Exception:
     # count = 0
-    count = int(count.decode("utf-8"))
+    count = count.decode("utf-8")
     print("{} was called {} times:".format(key, count))
 
     # Get inputs and outputs
-    inputs = redis.lrange("{}:inputs".format(key), 0, -1)
-    outputs = redis.lrange("{}:outputs".format(key), 0, -1)
+    inputs = store.lrange("{}:inputs".format(key), 0, -1)
+    outputs = store.lrange("{}:outputs".format(key), 0, -1)
     for inp, outp in zip(inputs, outputs):
         # try:
         #     inp = inp.decode("utf-8")
